e5e19ae7d4e64029fbddb78e778c4aefa61c5d5c
Dylan <dylan04herring@gmail.com>
2023-03-20 09:19:13 -0500

    Initial Commit

---
+++ src/main/java/com/unitproject/Unit/Project/Spring/UnitProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.unitproject.Unit.Project.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitProjectSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unitproject/Unit/Project/Spring/UnitProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitproject.Unit.Project.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
